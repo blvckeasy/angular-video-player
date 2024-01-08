0a3d659cf041b4688d1b9bca18befcd4eb607211
blvckeasy <islomabdurakhmonov8@gmail.com>
2024-01-08 23:12:41 +0500

    initilization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+      	AppComponent
+    ],
+    imports: [
+      	BrowserModule,
+      	AppRoutingModule
+    ],
+    providers: [
+      	provideClientHydration()
+    ],
+    bootstrap: [
+		AppComponent
+	]
 })
-export class AppModule { }
+export class AppModule {
+	
+}
